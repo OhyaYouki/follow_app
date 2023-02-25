4c766df6a0483727eefa8e832ce3d3a45e6d8d9d
OhyaYouki <88011584+OhyaYouki@users.noreply.github.com>
2023-02-25 21:14:30 +0900

    フォロワー数を非同期で変更

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,13 +1,35 @@
 class FollowsController < ApplicationController
-  before_action :set_user, only: [:create, :destroy]
+  before_action :set_user, only: [:create, :destroy, :user_follow, :user_follower]
 
   def create
     @follow = Relationship.create(user_id: current_user.id, following_id: params[:user_id])
+
+    # @userがフォローしている人たち
+    @following = Relationship.where(user_id: @user.id)
+
+    # @userをフォローしている人たち
+    @followed = Relationship.where(following_id: @user.id)
   end
 
   def destroy
     follow = Relationship.find(params[:id])
     follow.destroy
+
+    # @userがフォローしている人たち
+    @following = Relationship.where(user_id: @user.id)
+
+    # @userをフォローしている人たち
+    @followed = Relationship.where(following_id: @user.id)
+  end
+
+  def user_following
+    # @userがフォローしている人たち
+    @following = Relationship.where(user_id: @user.id)
+  end
+
+  def user_followed
+    # @userをフォローしている人たち
+    @followed = Relationship.where(following_id: @user.id)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :posts, only: [:index,:create]
   resources :users, only: [:show] do
     resources :follows, only: [:create,:destroy]
+    get 'user_following' ,to: 'follows#user_follow'
+    get 'user_followed' ,to: 'follows#user_follower'
   end
 
 end
