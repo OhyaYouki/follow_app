69d36d09d5e0dd69e8e3ae5890554cf47a31f1ab
OhyaYouki <88011584+OhyaYouki@users.noreply.github.com>
2023-02-25 21:14:17 +0900

    フォロワー数の表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,15 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
-    @follow = Relationship.find_by(user_id: current_user.id, following_id: @user)
+
     # 中間テーブルを用いたアソシエーションで値取ってこれる？
+    @follow = Relationship.find_by(user_id: current_user.id, following_id: @user)
+    
+    # @userがフォローしている人たち
+    @following = Relationship.where(user_id: @user.id)
+  
+    # @userをフォローしている人たち
+    @followed = Relationship.where(following_id: @user.id)
   end
 
 end
