f7d5299cd310c7cbc08f0670d78138f7d6cb7c26
OhyaYouki <88011584+OhyaYouki@users.noreply.github.com>
2023-03-11 20:16:07 +0900

    followsコントローラーリファクタリング

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,21 +1,15 @@
 class FollowsController < ApplicationController
   before_action :set_user
-  before_action :set_following  ,only: [:user_following]
-  before_action :set_followed   ,only: [:user_followed]
+  before_action :set_following  ,only: [:create,:destroy,:user_following]
+  before_action :set_followed   ,only: [:create,:destroy,:user_followed]
 
   def create
     @follow = Relationship.create(user_id: current_user.id, following_id: params[:user_id])
-
-    set_following
-    set_followed
   end
 
   def destroy
     follow = Relationship.find(params[:id])
     follow.destroy
-
-    set_following
-    set_followed 
   end
 
   def user_following
@@ -32,15 +26,13 @@ class FollowsController < ApplicationController
 
   def set_following
     # @userがフォローしている人たち
-    @following = @user.followings
-
     # @following = Relationship.where(user_id: @user.id)
+    @following = @user.followings
   end
 
   def set_followed
     # @userをフォローしている人たち
-    @followed = @user.followers    
-
     # @followed = Relationship.where(following_id: @user.id)
+    @followed = @user.followers
   end
 end
